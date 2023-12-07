74b68301284b8b686c27745ad128176541611cb5
Amol Kharat <amol.k@nexaitech.com>
2023-12-07 11:04:45 +0530

    I am adding cjanges

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { ContactComponent } from 'src/components/contact/contact.component';
 import { FooterComponent } from 'src/components/footer/footer.component';
 import { NavbarComponent } from 'src/components/navbar/navbar.component';
 import { LoginComponent } from 'src/components/login/login.component';
+import { authGuard } from './guards/auth.guard';
 
 
 const routes: Routes = [
@@ -21,7 +22,7 @@ const routes: Routes = [
   {path:'contact',component:ContactComponent},
   {path:'footer',component:FooterComponent},
   {path:'navbar',component:NavbarComponent},
- {path:'admin',loadChildren:()=>import('./admin/admin.module').then((m)=>m.AdminModule)}
+ {path:'admin',loadChildren:()=>import('./admin/admin.module').then((m)=>m.AdminModule), canActivate: [authGuard]}
 ,
  {path:'login',component:LoginComponent} 
 ];
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DynamicEngg';
+  private router : Router;
+  hideNavbar: boolean = false;
+  constructor (router : Router)
+  {
+    this.router = router;
+  }
+  ngOnInit(): void {
+    this.router.events
+      .pipe(
+        // Filter only NavigationEnd events
+        filter((event) => event instanceof NavigationEnd)
+      )
+      .subscribe((event:any) => {
+        // Get the current URL
+        const currentUrl = event.url;
+
+        // Check if the current URL contains "admin or login"
+        const isAdminRoute = currentUrl.includes('admin')||currentUrl.includes('login');
+
+        // Use the isAdminRoute value to control the visibility of the navbar
+        this.hideNavbar = !isAdminRoute;
+      });
+  }
 }
+
+
