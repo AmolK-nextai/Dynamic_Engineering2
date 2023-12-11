c30aaa5d15cde309f4fda9d92dc5772013a91f17
vaibhav-nexai <134479358+vaibhav-nexai@users.noreply.github.com>
2023-12-11 17:24:15 +0530

    ui updates
    changed color
    navbar added toggle bar

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  ngOnInit() {
+    AOS.init();
+  }
+  ngAfterViewInit() {
+    AOS.init();
 
+  }
 }
