de2c39eea22e3fa8f0916987f4a3367d2bd25cf9
vaibhav-nexai <134479358+vaibhav-nexai@users.noreply.github.com>
2023-12-12 13:31:33 +0530

    carousel updates

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import * as AOS from 'aos';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent  {
   ngOnInit() {
     AOS.init();
   }
   ngAfterViewInit() {
     AOS.init();
+    
 
   }
 }
